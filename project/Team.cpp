fc3bf2831adc750cf27202c33ec47eca788aab70
mdomag <126866195+mdomag@users.noreply.github.com>
2024-05-28 01:30:10 +0200

    Update Team.cpp

--- project/Team.cpp
+++ project/Team.cpp
@@ -7,16 +7,16 @@ void Team::changeName(string name) {
 void Team::change() {
     
 }
-<<<<<<< HEAD
+
 void Team::showInfo() {
     printf("Team name: %s\n", name.c_str());
     for(Character character: team) {
         character.showInfo();
     }
     printf("Team level: %d\n", level);
-=======
+
 
 int Team::getTeamSize() {
     return team.size();
->>>>>>> origin/main
+
 }
