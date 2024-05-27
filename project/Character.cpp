b02162dcadea3ed76b95db76a49bcd169f538458
Maria Ciężak <127038795+mmciezak@users.noreply.github.com>
2024-05-28 01:45:52 +0200

    dodatkowe funkcje pomocnicze

--- project/Character.cpp
+++ project/Character.cpp
@@ -8,3 +8,15 @@ int Character::getLevel(){
     return this->level;
 }
 
+std::string Character::getName(){
+    return this->name;
+}
+
+int Character::getId(){
+    return this->id;
+}
+
+std::string Character::getCode(){
+    return this->code;
+}
+
