35684834812b5bcd393886e6b12866c7c14408c5
Maria Ciężak <127038795+mmciezak@users.noreply.github.com>
2024-05-27 23:25:15 +0200

    getLevel()

--- project/Character.h
+++ project/Character.h
@@ -7,6 +7,7 @@ using namespace std;
 class Character {
 public:
     void showInfo();
+    int getLevel();
     Character(int i, int h, string n, string c, int l, int d): id(i), hp(h), name(n), code(c), level(l), dmg(d) {}
 private:
     int id;
